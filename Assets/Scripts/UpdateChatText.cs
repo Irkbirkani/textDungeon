38f2019193714c595d3b1b970d68a3e115b33533
Irkbirkani <aturner3@trinity.edu>
2017-10-12 02:18:50 -0500

    Working on TextInput

--- Assets/Scripts/UpdateChatText.cs
+++ Assets/Scripts/UpdateChatText.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 
 public class UpdateChatText : MonoBehaviour {
 
-	public Text OutputText;
-	public InputField input;
+    public Text[] OutputText;
+    public InputField input;
 
-	private string[] ChatText;
+    private string[] ChatText;
+    private bool wasFocused;
 
-	// Use this for initialization
-	void Start () {
-		ChatText = new string[7] { "\n", "\n", "\n", "\n", "\n", "\n", "\n", };
+    // Use this for initialization
+    void Start() {
+        ChatText = new string[9] { "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n" };
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (input.isFocused)
+        {
+            printChatText();
+        }
+        wasFocused = input.isFocused;
+    }
+
+
+    public void UpdateText() {
+        updateChatText(input.text + "\n");
+        input.text = "";
+        input.ActivateInputField();
+    }
+
+    void updateChatText(string newText)
+    {
+        if (newText != "")
+        {
+            for (int i = 0; i <= ChatText.Length - 2; i++)
+            {
+                ChatText[i] = ChatText[i + 1];
+            }
+            ChatText[ChatText.Length - 1] = newText;
+        }
+    }
+
+	 void printChatText () {
+        for (int i = 0; i <= ChatText.Length-1; i++)
+            OutputText[i].text = ChatText[i];
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		OutputText.text = printChatText();
-	}
 
-
-	public void UpdateText(){
-
-		updateChatText(input.text+ "\n");
-		input.text = "";
-		input.ActivateInputField ();
-	}
-
-	void updateChatText(string newText){
-		for (int i = 0; i <= ChatText.Length - 2; i++) {
-			ChatText [i] = ChatText [i + 1];
-		}
-	
-		ChatText [ChatText.Length-1] = newText;
-}
-
-	 string printChatText () {
-		string temp = "";
-		for (int i = 0; i <= ChatText.Length-1; i++)
-		{
-			temp = temp + ChatText[i];
-		}
-		return temp;
-	}
+    void OnMouseDown()
+    {
+        
+    }
 }
