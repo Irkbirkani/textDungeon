bf8de54c72c9d88ffb81ab5f066632f003a9d271
Irkbirkani <aturner3@trinity.edu>
2018-09-15 21:38:36 -0500

    Started base for loading location and such from files.

--- Assets/Scripts/Utility/InitialSetup.cs
+++ Assets/Scripts/Utility/InitialSetup.cs
@@ -20,4 +20,39 @@ public class InitialSetup : MonoBehaviour {
 
         GameObject.Find ("Player").GetComponent<Entity>().SetLocation(startRoom);
     }
+
+    void LoadLocation(string locationPath)
+    {
+        var Roomfile = Resources.Load<TextAsset>(locationPath + "RoomData");
+        var Itemfile = Resources.Load<TextAsset>(locationPath + "ItemData");
+        var Enemyfile = Resources.Load<TextAsset>(locationPath + "EnemyData");
+        CreateRooms(Roomfile);
+        PlaceEnemies(Enemyfile);
+        PlaceItems(Itemfile);
+    }
+
+    void CreateRooms(TextAsset rooms)
+    {
+        string[] lines = rooms.text.Split('\n');
+        foreach(string s in lines)
+        {
+            string[] paramaters = s.Split(',');
+            var room = Instantiate(Resources.Load("Room", typeof(GameObject))) as GameObject;
+        }
+    }
+
+    void PlaceEnemies(TextAsset enemy)
+    {
+
+    }
+
+    void PlaceNPCS()
+    {
+
+    }
+
+    void PlaceItems(TextAsset items)
+    {
+
+    }
 }
