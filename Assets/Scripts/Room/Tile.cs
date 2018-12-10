350ec3bbd892ec86dd5f0a5ecd8b6e44622d8cab
Asa Turner <asaturner@Asas-MBP.attlocal.net>
2018-12-10 10:58:25 -0600

    Started adding step support

---
+++ Assets/Scripts/Room/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Transform up, down, left, right;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Transform GetSide(string side)
+    {
+        switch (side)
+        {
+            case "up":
+                return up;
+            case "down":
+                return down;
+            case "left":
+                return left;
+            case "right":
+                return right;
+            default:
+                return null;
+        }
+    }
+}
